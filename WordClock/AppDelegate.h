aeeff78ef4e4eab3d3d812642df6324f7bcff617
helloyou2012 <helloyou2012@yeah.net>
2013-12-25 22:16:42 +0800

    Initial Commit

---
+++ WordClock/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WordClock
+//
+//  Created by ZhenzhenXu on 12/25/13.
+//  Copyright (c) 2013 ZhenzhenXu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WordClock/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  WordClock
+//
+//  Created by ZhenzhenXu on 12/25/13.
+//  Copyright (c) 2013 ZhenzhenXu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
